23df005f64ae69adf6eb8322455283b69085814a
Fradgile <fradgile@gmail.com>
2016-01-09 12:00:21 -0600

    feat: Completed basic requirements of resume project.

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span>%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -53,7 +53,8 @@ var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+//var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL = '<br><a href="%data%">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -64,8 +65,10 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+    var iName = inName($('#name').text());
+
+    $('#name').html(iName);
+    console.log("iName = " + iName);
   });
 });
 
@@ -84,11 +87,9 @@ function logClicks(x,y) {
   console.log('x location: ' + x + '; y location: ' + y);
 }
 
-$(document).click(function(loc) {
-  // your code goes here!
+$(document).click(function(loc){
+  logClicks(loc.pageX, loc.pageY);
 });
-
-
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
@@ -109,9 +110,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -129,16 +130,20 @@ function initializeMap() {
     locations.push(bio.contacts.location);
 
     // iterates through school locations and appends each location to
-    // the locations array
-    for (var school in education.schools) {
-      locations.push(education.schools[school].location);
-    }
+    // the locations array. Note that forEach is used for array iteration
+    // as described in the Udacity FEND Style Guide:
+    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    education.schools.forEach(function(school){
+      locations.push(school.location);
+    });
 
     // iterates through work locations and appends each location to
-    // the locations array
-    for (var job in work.jobs) {
-      locations.push(work.jobs[job].location);
-    }
+    // the locations array. Note that forEach is used for array iteration
+    // as described in the Udacity FEND Style Guide:
+    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    work.jobs.forEach(function(job){
+      locations.push(job.location);
+    });
 
     return locations;
   }
@@ -173,6 +178,8 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map, marker);
+
     });
 
     // this is where the pin actually gets added to the map.
@@ -205,19 +212,19 @@ function initializeMap() {
     var service = new google.maps.places.PlacesService(map);
 
     // Iterates through the array of locations, creates a search object for each location
-    for (var place in locations) {
+    locations.forEach(function(place){
 
       // the search request object
       var request = {
-        query: locations[place]
+       query: place
       };
 
       // Actually searches the Google Maps API for location data and runs the callback
       // function with the search results after each search.
       service.textSearch(request, callback);
-    }
-  }
 
+    });
+  }
   // Sets the boundaries of the map based on pin locations
   window.mapBounds = new google.maps.LatLngBounds();
 
@@ -235,11 +242,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,222 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+ 	"name"		: "Fergus Emmett",
+ 	"role"		: "Web Developer",
+ 	"contacts": {
+ 		"mobile"	: "(555) 555 5555",
+ 		"email"		: "fradgile@gmail.com",
+    "github"  : "https://github.com/fradgile",
+ 		"twitter"	: "@fradgile",
+ 		"location"	: "Texas"
+ 	},
+  "welcomeMessage": "Certified IT professional with experience in design, development and implementation of Oracle database solutions.",
+ 	"skills": ["Oracle", "PL/SQL", "APEX", "jQuery"],
+ 	"biopic"	: "images/FergCrop.jpg"
+ };
+bio.display = function(){
+  var formattedName     = HTMLheaderName.replace('%data%', bio.name);
+  var formattedRole     = HTMLheaderRole.replace('%data%', bio.role);
+
+  var formattedMobile   = HTMLmobile.replace('%data%', bio.contacts.mobile);
+  var formattedEmail    = HTMLemail.replace('%data%', bio.contacts.email);
+  var formattedTwitter  = HTMLtwitter.replace('%data%', bio.contacts.twitter);
+  var formattedGithub   = HTMLgithub.replace('%data%', bio.contacts.github);
+  var formattedLocation   = HTMLlocation.replace('%data%', bio.contacts.location);
+
+  var formattedBioPic   = HTMLbioPic.replace('%data%', bio.biopic);
+  var formattedWelcomeMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+
+  $("#header").prepend(formattedName, formattedRole);
+
+  $("#topContacts").append(formattedMobile, formattedEmail, formattedGithub, formattedTwitter, formattedLocation);
+  $("#footerContacts").append(formattedMobile, formattedEmail, formattedGithub, formattedTwitter, formattedLocation);
+
+  $("#header").append(formattedBioPic, formattedWelcomeMsg);
+
+  if(bio.skills.length > 0){
+    $("#header").append(HTMLskillsStart);
+    for (skill in bio.skills) {
+      $("#skills").append(HTMLskills.replace('%data%', bio.skills[skill]));
+    };
+  };
+};
+
+
+var work = {
+  "jobs": [
+    {
+      "employer": "University Support Services",
+      "title": "Software Developer",
+      "location": "Great River, New York",
+      "dates": "2012 - 2015",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "Estee Lauder Companies",
+      "title": "Senior Developer of Business Applications",
+      "location": "Melville, New York",
+      "dates": "2006 - 2012",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    }
+  ]
+};
+work.display = function()
+{
+  for (job in work.jobs) {
+      $("#workExperience").append(HTMLworkStart);
+
+      var formattedJob = ( HTMLworkEmployer.replace('%data%', work.jobs[job].employer) +
+                              HTMLworkTitle.replace('%data%', work.jobs[job].title));
+      formattedJob += HTMLworkDates.replace('%data%', work.jobs[job].dates);
+      formattedJob += HTMLworkLocation.replace('%data%', work.jobs[job].location);
+      formattedJob += HTMLworkDescription.replace('%data%', work.jobs[job].description);
+
+      $(".work-entry:last").append(formattedJob);
+  };
+};
+
+var education = {
+    "schools": [
+    {
+      "name": "Hofstra University",
+      "location": "Hempstead, New York",
+      "degree": "MBA",
+      "majors": ["Finance"],
+      "dates": "2010",
+      "url": "www.hofstra.edu"
+    },
+    {
+      "name": "University of Limerick",
+      "location": "Limerick, Ireland",
+      "degree": "Graduate Diploma in Computing",
+      "majors": [""],
+      "dates": "1998",
+      "url": "www.ul.ie"
+    },
+    {
+      "name": "Dublin City University",
+      "location": "Dublin, Ireland",
+      "degree": "B.Sc. (Hons) Biotechnology",
+      "majors": [""],
+      "dates": "1996",
+      "url": "www.dcu.ie"
+    }
+    ],
+    "onlineCourses": [
+    {
+      "title": "Front-End JavaScript Frameworks: AngularJS",
+      "school": "Coursera",
+      "date": "2015",
+      "url": "https://www.coursera.org/learn/angular-js"
+    },
+    {
+      "title": "Front-End Web UI Frameworks and Tools",
+      "school": "Coursera",
+      "date": "2015",
+      "url": "https://www.coursera.org/learn/web-frameworks"
+    },
+    {
+      "title": "HTML, CSS and JavaScript",
+      "school": "Coursera",
+      "date": "2015",
+      "url": "https://www.coursera.org/learn/html-css-javascript"
+    },
+    {
+      "title": "Mobile Cloud Computing with Android",
+      "school": "Coursera",
+      "date": "2015",
+      "url": "https://www.coursera.org/account/accomplishments/specialization/KKV4XTNBUDQ5"
+    },
+    {
+      "title": "R Programming",
+      "school": "Coursera",
+      "date": "2015",
+      "url": "https://www.coursera.org/account/accomplishments/records/Yuyh2JP4phGGns8p"
+    },
+    {
+      "title": "Web Application Architectures",
+      "school": "Coursera",
+      "date": "2014",
+      "url": "https://www.coursera.org/account/accomplishments/records/8YJLfAgW6sxahUdf"
+    }
+    ]
+};
+education.display = function(){
+  for (school in education.schools) {
+      $("#education").append(HTMLschoolStart);
+      var formattedSchool = ( HTMLschoolName.replace('%data%', education.schools[school].name) +
+                              HTMLschoolDegree.replace('%data%', education.schools[school].degree));
+      formattedSchool += HTMLschoolDates.replace('%data%', education.schools[school].dates);
+      formattedSchool += HTMLschoolLocation.replace('%data%', education.schools[school].location);
+
+      for (major in education.schools[school].majors) {
+        formattedSchool += HTMLschoolMajor.replace('%data%', education.schools[school].majors[major]);
+      }
+      $(".education-entry:last").append(formattedSchool);
+  };
+
+  if(education.onlineCourses.length > 0){
+    $("#education").append(HTMLonlineClasses);
+
+    for (course in education.onlineCourses) {
+      $("#education").append(HTMLschoolStart);
+      var formattedCourse = ( HTMLonlineTitle.replace('%data%', education.onlineCourses[course].title) +
+                              HTMLonlineSchool.replace('%data%', education.onlineCourses[course].school));
+      formattedCourse += HTMLonlineDates.replace('%data%', education.onlineCourses[course].date);
+      formattedCourse += HTMLonlineURL.replace(/%data%/gi, education.onlineCourses[course].url);
+
+      $(".education-entry:last").append(formattedCourse);
+    };
+
+  };
+};
+
+var projects = {
+    "projects": [
+      {
+        "title": "Technical Project 1",
+        "dates": "2014 - 2015",
+        "description": "This is a sample description of a project that I will later place here.",
+        "images": ["http://lorempixel.com/image_output/technics-q-c-200-200-1.jpg", "http://lorempixel.com/image_output/technics-q-c-200-200-6.jpg"]
+      },
+      {
+        "title": "Another Technical Project",
+        "dates": "2013 - 2014",
+        "description": "This is a sample description of a project that I will later place here.",
+        "images": ["http://lorempixel.com/image_output/technics-q-c-200-200-4.jpg", "http://lorempixel.com/image_output/technics-q-c-200-200-9.jpg"]
+      }
+    ]
+};
+projects.display = function(){
+  for (project in projects.projects) {
+      $("#projects").append(HTMLprojectStart);
+      var formattedProject = HTMLprojectTitle.replace('%data%', projects.projects[project].title);
+      formattedProject += HTMLprojectDates.replace('%data%', projects.projects[project].dates);
+      formattedProject += HTMLprojectDescription.replace('%data%', projects.projects[project].description);
+
+      for (image in projects.projects[project].images) {
+        formattedProject += HTMLprojectImage.replace('%data%', projects.projects[project].images[image]);
+      }
+      $(".project-entry:last").append(formattedProject);
+  };
+};
+
+// Call the display functions to add content to index.html
+bio.display();
+work.display();
+education.display();
+projects.display();
+
+$("#mapDiv").append(googleMap);
+
+
+$("#main").append(internationalizeButton);
+
+function inName(name){
+  var nameArray = name.split(" ");
+  var firstName = nameArray[0];
+  var lastName  = nameArray[1];
+  firstName = firstName.toLowerCase();
+  firstName = firstName[0].toUpperCase() + firstName.slice(1);
+  lastName = lastName.toUpperCase();
+  return firstName + " " + lastName;
+};
